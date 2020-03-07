3146c112ce2a9392e6bc6327792a0182b55afc12
Evgeniy Reizner <razrfalcon@gmail.com>
2020-03-07 11:04:02 +0200

    Add missing file.

---
+++ test-all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# build without logging
+cargo build --no-default-features
+# build with logging
+cargo build
+
+cargo test
+
+cd c-api
+cargo build
+gcc test.c -o test -L./target/debug/ -lttfparser -Werror
+env LD_LIBRARY_PATH=./target/debug/ ./test

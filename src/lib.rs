36023f84a1da29d04a212670f8f6e61c10cedae7
Max Melander <max@modyfi.io>
2024-10-04 00:20:00 +0800

    handle multiple axes with same tag

--- src/lib.rs
+++ src/lib.rs
@@ -2320,16 +2320,13 @@ impl<'a> Face<'a> {
             return None;
         }
 
-        if let Some((i, var_axis)) = self
-            .variation_axes()
-            .into_iter()
-            .enumerate()
-            .find(|(_, var_axis)| var_axis.tag == axis)
-        {
-            self.coordinates.data[i] = var_axis.normalized_value(value);
+        for (i, var_axis) in self.variation_axes().into_iter().enumerate() {
+            if var_axis.tag == axis {
+                self.coordinates.data[i] = var_axis.normalized_value(value);
 
-            if let Some(avar) = self.tables.avar {
-                let _ = avar.map_coordinate(self.coordinates.as_mut_slice(), i);
+                if let Some(avar) = self.tables.avar {
+                    let _ = avar.map_coordinate(self.coordinates.as_mut_slice(), i);
+                }
             }
         }
 
